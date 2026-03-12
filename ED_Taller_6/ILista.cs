ab2be4af7b5cee4d17baee963fca09bd490de170
CazadorDeVocachicos <jjsalazar@uniboyaca.edu.co>
2026-03-12 12:02:04 -0500

    ilista
    ilista

---
+++ ED_Taller_6/ILista.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EST_estructuras
+{
+    internal interface ILista<T>
+    {
+        void AddFirst(T dato);
+        void AddLast(T dato);
+        bool Contains(T dato);
+        int Count();
+        T GetAt(int index);
+        void Insert(int index, T dato);
+        void Print();
+        void RemoveFirst();
+        void RemoveLast();
+        bool Remove(T dato);
+
+    }
+}
