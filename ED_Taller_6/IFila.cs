dd5c059ebdffc064bb5ddeeb7c1393a43756070f
CazadorDeVocachicos <jjsalazar@uniboyaca.edu.co>
2026-03-12 12:07:50 -0500

    a

---
+++ ED_Taller_6/IFila.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ED_Taller_6
+{
+    internal interface IFila<T>
+    {
+        void Enqueue(T item);
+        T Dequeue();
+        T Peek();
+        bool IsEmpty();
+        int Count();
+        void Clear();
+    }
+}
---
+++ ED_Taller_6/IPila.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ED_Taller_6
+{
+    internal interface IPila<T>
+    {
+        void Push(T item);
+        T Pop();
+        T Peek();
+        bool IsEmpty();
+        int Count();
+        void Clear();
+    }
+}
